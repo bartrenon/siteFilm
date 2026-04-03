44287330a6223a0ad05d76c7ce3ca3226ba05375
Renon bart <b.renon@mail.com>
2026-04-03 14:58:27 +0200

    Ajoutez des fichiers projet.

---
+++ siteFilm/Controllers/FilmController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using siteFilm.Models;
+
+namespace siteFilm.Controllers;
+
+public class FilmController : Controller
+{
+    public readonly List<Film> Films = new List<Film>
+    {
+        new Film { Id = 1, Titre = "Inception", Genre = "Science-Fiction", Annee = 2010 },
+        new Film { Id = 2, Titre = "The Dark Knight", Genre = "Action", Annee = 2008 },
+        new Film { Id = 3, Titre = "Interstellar", Genre = "Science-Fiction", Annee = 2014 },
+        new Film { Id = 4, Titre = "Forrest Gump", Genre = "Drame", Annee = 1994 },
+        new Film { Id = 5, Titre = "Gladiator", Genre = "Action", Annee = 2000 },
+        new Film { Id = 6, Titre = "La La Land", Genre = "Comédie Musicale", Annee = 2016 }
+    };
+
+    public IActionResult Index(string? genre = null) 
+    {
+        List<Film> films = string.IsNullOrEmpty(genre) ? Films :
+           Films.Where(f => f.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase)).ToList();
+
+        GetGenres();
+
+        return View(films);
+    }
+
+    public IActionResult IndexTriCroissant()
+    {
+        var films = Films.OrderBy(f => f.Titre).ToList();
+        return View("Index", films);
+    }
+
+
+    public IActionResult Details(int id)
+    {
+        Film? film = Films.FirstOrDefault(f => f.Id == id);
+
+        if (film is null)
+        {
+            TempData["Erreur"] = "Le film demandé n'existe pas.";
+            return RedirectToAction("Index");
+        }
+
+        TempData["Info"] = $"Vous consultez les détails du film : {film.Titre}";
+
+        GetGenres();
+
+        return View(film);
+    }
+
+
+    public IActionResult APropos() 
+    {
+      
+        ViewData["TotalFilms"] = Films.Count;
+        ViewBag.NbGenres = Films.Select(f => f.Genre).Distinct().Count();
+        ViewData["PlusAncien"] = Films.Min(f => f.Annee);
+        ViewData["PlusRecent"] = Films.Max(f => f.Annee);
+
+        var message = TempData["Message"];
+        ViewBag.Message = message;
+
+        GetGenres();
+
+        return View();
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var film = Films.FirstOrDefault(f => f.Id == id);
+        if (film == null)
+            return NotFound();
+
+        Films.Remove(film);
+
+        TempData["success"] = $"Le film « {film.Titre} » a été supprimé avec succès.";
+
+        return RedirectToAction("Index");
+    }
+
+
+    private void GetGenres()
+    {
+        ViewBag.Genres =  Films.Select(f => f.Genre).Distinct().ToList();
+    }
+
+
+}
---
+++ siteFilm/Models/Film.cs
@@ -0,0 +1,9 @@
+﻿namespace siteFilm.Models;
+
+public class Film
+{
+   public int Id { get; set; }
+   public string Titre { get; set; } = string.Empty;
+   public string Genre { get; set; } = string.Empty;
+   public int Annee { get; set; }
+}
